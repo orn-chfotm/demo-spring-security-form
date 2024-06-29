67b86a657d498fa863ed9adf53129f811e513891
orn-chfotm <chfotm@gmail.com>
2024-06-29 13:44:01 +0900

    git init commit inflearn Spring Security

---
+++ src/main/java/com/inflearn/demospringsecurityform/account/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.inflearn.demospringsecurityform.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+
+    Account findByUsername(String username);
+}
