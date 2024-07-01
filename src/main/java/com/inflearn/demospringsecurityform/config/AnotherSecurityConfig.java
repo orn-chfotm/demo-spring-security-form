b4506f6a411aaecc94ff5284c364f1b26289f1a1
dh_kim <dh_kim@easymedia.net>
2024-07-01 14:46:36 +0900

    Authenticaiton

---
+++ src/main/java/com/inflearn/demospringsecurityform/config/AnotherSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.inflearn.demospringsecurityform.config;
+
+public class AnotherSecurityConfig {
+
+}
+//public class AnotherSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//            .antMatcher("/account/**")
+//                .authorizeRequests()
+//                .anyRequest().permitAll()
+//        ;
+//    }
+//
+//}
